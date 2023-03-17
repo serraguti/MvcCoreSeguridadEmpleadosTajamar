c5c9aee40103bc84dc0a5e2efe83faf9d0c3bfc1
serraguti <pacoserranox@gmail.com>
2023-03-17 09:29:05 +0100

    Add project files.

---
+++ MvcCoreSeguridadEmpleados/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,35 @@
+﻿using MvcCoreSeguridadEmpleados.Data;
+using MvcCoreSeguridadEmpleados.Models;
+
+namespace MvcCoreSeguridadEmpleados.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private EmpleadosContext context;
+
+        public RepositoryEmpleados(EmpleadosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Empleado> GetEmpleados()
+        {
+            return this.context.Empleados.ToList();
+        }
+
+        public Empleado FindEmpleado(int idempleado)
+        {
+            return
+                this.context.Empleados.FirstOrDefault
+                (x => x.IdEmpleado == idempleado);
+        }
+
+        public List<Empleado> GetEmpleadosDepartamento(int iddepartamento)
+        {
+            var consulta = from datos in this.context.Empleados
+                           where datos.Departamento == iddepartamento
+                           select datos;
+            return consulta.ToList();
+        }
+    }
+}
